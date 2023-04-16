0f917b34e1f236ba47d5d1bca3610f835a8870c3
aslenok <130287435+aslenok@users.noreply.github.com>
2023-04-16 17:29:53 +0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -2,18 +2,51 @@ from PyQt5.QtWidgets import *
 
 app = QApplication([])
 window = QWidget()
-window.setWindowTitle('Калькулятор хомячьей электроэнергетики')
 window.resize(500,500)
+window.setWindowTitle('Калькулятор хомячьей электроэнергетики')
 
 mainline = QVBoxLayout()
 window.setLayout(mainline)
 
-line1 = QVBoxLayout()
-line2 = QVBoxLayout()
+inp1 = QLineEdit()
+inp1.setPlaceholderText('Введите массу хомяка в кг')
+
+inp2 = QLineEdit()
+inp2.setPlaceholderText('Введите диаметр колеса в м')
+
+inp3 = QLineEdit()
+inp3.setPlaceholderText('Введите количество хомяков')
+
+def raschet():
+    massa = inp1.text()
+    dia = inp2.text()
+    kol = inp3.text()
+    dia = int(dia)
+    kol = int(kol)
+    massa = int(massa)
+    dlina = dia * 3.14
+    nrg = massa * 10 * dlina * kol
+
+mainline = QVBoxLayout()
+
+line1 = QHBoxLayout()
 mainline.addLayout(line1)
+
+line2 = QHBoxLayout()
 mainline.addLayout(line2)
 
-text1 = QLabel('P.s. данные для расчета были взяты при средней массе ')
+line3 = QHBoxLayout()
+mainline.addLayout(line3)
+
+line4 = QHBoxLayout()
+mainline.addLayout(line4)
+
+line5 = QHBoxLayout()
+mainline.addLayout(line5)
+
+line6 = QHBoxLayout()
+mainline.addLayout(line6)
+
 
 window.show()
 app.exec_()
