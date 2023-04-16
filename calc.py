de7c9ec2baecfbb03f7139ae1a84902e07a95f5d
user <130287380+vinaapo@users.noreply.github.com>
2023-04-16 17:09:13 +0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import *
 app = QApplication([])
 window = QWidget()
 window.setWindowTitle('Калькулятор хомячьей электроэнергетики')
+window.resize(500,500)
 
 mainline = QVBoxLayout()
 window.setLayout(mainline)
@@ -14,3 +15,5 @@ mainline.addLayout(line2)
 
 text1 = QLabel('P.s. данные для расчета были взяты при средней массе ')
 
+window.show()
+app.exec_()
