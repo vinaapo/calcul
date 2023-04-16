f4575fe3649c9bb329f75428389aedc236101949
aslenok <130287435+aslenok@users.noreply.github.com>
2023-04-16 17:24:32 +0500

    Update вычисления.py

--- вычисления.py
+++ вычисления.py
@@ -1,15 +1,22 @@
-inp1 = QLineEdit('Введите массу хомяка в кг')
-massa = inp1.text()
-massa = int(massa)
+inp1 = QLineEdit()
+inp1.setPlaceholderText('Введите массу хомяка в кг')
 
-inp2 = QLineEdit('Введите диаметр колеса в м')
-dia = inp2.text()
-dia = int(dia)
 
-dlina = dia * 3.14
+inp2 = QLineEdit()
+inp2.setPlaceholderText('Введите диаметр колеса в м')
 
-inp3 = QLineEdit('Введите количество хомяков')
-kol = inp3.text()
-kol = int(kol)
 
-nrg = massa * 10 * dlina * kol
+inp3 = QLineEdit()
+inp3.setPlaceholderText('Введите количество хомяков')
+
+
+
+def raschet():
+    massa = inp1.text()
+    dia = inp2.text()
+    kol = inp3.text()
+    dia = int(dia)
+    kol = int(kol)
+    massa = int(massa)
+    dlina = dia * 3.14
+    nrg = massa * 10 * dlina * kol
