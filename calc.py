a9b27ee16cd8b0e074a7b8cd8ab269764ccb1621
aslenok <130287435+aslenok@users.noreply.github.com>
2023-04-23 17:23:35 +0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -17,7 +17,15 @@ inp2.setPlaceholderText('Введите диаметр колеса в м')
 inp3 = QLineEdit()
 inp3.setPlaceholderText('Введите количество хомяков')
 
-def raschet():
+text1 = QLabel('''Для подсчета итогового кооличества вырабатываемой энергии введите данные в полях. 
+P.s. результат считался по формуле N = mgS. (N - энергия, m - масса, g - ускорение, S - путь. Путь (S) = диаметр колеса * 3,14 (число ПИ)''')
+
+button1 = QPushButton('Произвести расчёты')
+
+text2 = QLabel('Результат:')
+
+
+def calculate():
     massa = inp1.text()
     dia = inp2.text()
     kol = inp3.text()
@@ -26,9 +34,12 @@ def raschet():
     massa = int(massa)
     dlina = dia * 3.14
     nrg = massa * 10 * dlina * kol
+    text2 = QLabel('Результат:', nrg, 'Ватт')
+    line6.addWidget(text2)
 
 line1 = QHBoxLayout()
 mainline.addLayout(line1)
+line1.addWidget(text1)
 
 line2 = QHBoxLayout()
 mainline.addLayout(line2)
@@ -44,10 +55,11 @@ line4.addWidget(inp3)
 
 line5 = QHBoxLayout()
 mainline.addLayout(line5)
+line5.addWidget(button1)
 
 line6 = QHBoxLayout()
 mainline.addLayout(line6)
-
+line6.addWidget(text2)
 
 window.show()
 app.exec_()
